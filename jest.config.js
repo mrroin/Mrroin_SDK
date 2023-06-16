aa721b7f9ef788ea2e791ffe7311a3e7e00ea796
Rogelio Mendez <48691087+mrroin@users.noreply.github.com>
2023-06-16 12:51:09 -0600

    Update jest.config.js for testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,23 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  verbose: true,
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "\\.[j]sx?$": "babel-jest",
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  collectCoverageFrom: ["src/**/*.{jsx,ts,tsx}", "!src/polyfills.ts"],
+  testRegex: "(\\.|/)(test|spec)\\.(jsx?|tsx?)$",
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!intl-messageformat|intl-messageformat-parser).+\\.js$",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
